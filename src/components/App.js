f339fef08a01ecd36f150dc69b9c5d8e7aa8141f
Luke Schoen <ltfschoen@gmail.com>
2016-11-27 12:32:00 +1100

    Extracted header into separate Header Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 // App Component handles template on all pages
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import Header from './common/Header';
 
 // Class instead of Stateless component to support hot reloading at top-level
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header</p>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/about">About</Link>
+        <Header />
         {/* React Router passes Child Components as Props of App Component*/}
         {this.props.children}
       </div>
