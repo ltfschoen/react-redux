a780ec6a27b842713e6265f8484daf30c1fed963
Luke Schoen <ltfschoen@gmail.com>
2016-12-01 06:36:18 +1100

    Updated skill Reducer using spread operator so not change state when processing

--- src/reducers/skillReducer.js
+++ src/reducers/skillReducer.js
@@ -1,10 +1,16 @@
 export default function skillReducer(state = [], action) {
   switch(action.type) {
     case 'CREATE_SKILL':
-      // WRONG - do not mutate state
-      state.push(action.skill);
-      return state;
+      /** Avoid mutating state by using ES6 spread operator
+       *  to return new instance of State array, then create deep copy of
+       *  the passed in action.skill and return a brand new State containing
+       *  extra value of new action.skill
+       */
+      return [...state,
+        Object.assign({}, action.skill)
+      ];
     default:
+      // Return existing State
       return state;
   }
 }
