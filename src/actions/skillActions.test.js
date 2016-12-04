271ee42825a229ddc750d369836f9ab97e9e6242
Luke Schoen <ltfschoen@gmail.com>
2016-12-04 11:15:21 +1100

    Added Thunk tests using Enzyme

--- src/actions/skillActions.test.js
+++ src/actions/skillActions.test.js
@@ -2,6 +2,7 @@ import expect from 'expect';
 import * as skillActions from './skillActions';
 import * as types from './actionTypes';
 
+// Mock Store (using redux-mock-store) and HTTP Calls (using nock)
 import thunk from 'redux-thunk';
 import nock from 'nock';
 import configureMockStore from 'redux-mock-store';
@@ -22,6 +23,35 @@ describe('Skill Actions', () => {
 
       // assert
       expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should create BEGIN_AJAX_CALL and LOAD_SKILLS_SUCCESS when loading skills', (done) => {
+    // Example call to nock.
+    // nock('http://example.com/')
+    //   .get('/skills')
+    //   .reply(200, { body: { skill: [{ id: 'luke-schoen', fullName: 'Luke Schoen'}] }});
+
+    const expectedActions = [
+      {type: types.BEGIN_AJAX_CALL},
+      {type: types.LOAD_SKILLS_SUCCESS, body: {skills: [{id: 'clean-code', skillName: 'Clean Code'}]}}
+    ];
+
+    const store = mockStore({skills: []}, expectedActions);
+    store.dispatch(skillActions.loadSkills()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(types.LOAD_SKILLS_SUCCESS);
+      done();
     });
   });
 });
